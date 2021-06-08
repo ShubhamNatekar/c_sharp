3e0aa0a7a0b1c33ce71f0be2de298245cdb37398
shubhamnatekar <shubhamnatekar9221@gmail.com>
2021-06-08 17:30:49 +0530

    Understand the Method Overloading

---
+++ Overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Overloading
+{   
+    public class Cal
+    {  
+        public static int add(int a,int b)
+        {  
+            return a + b;  
+        }
+        //adding extra parameter  
+        public static int add(int a, int b, int c)  
+        {  
+            return a + b + c;  
+        }
+
+        //changing data type
+         public static float add(float a, float b)  
+        {  
+            return a + b;  
+        }  
+    }  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cal.add(12, 23));  
+            Console.WriteLine(Cal.add(12, 23, 25));
+            Console.WriteLine(Cal.add(12.4f,21.3f));  
+        }
+    }
+}
