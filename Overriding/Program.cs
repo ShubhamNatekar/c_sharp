dfa12fce89e6ea1419c91f4ee6918662bc1d4c3a
shubhamnatekar <shubhamnatekar9221@gmail.com>
2021-06-08 17:31:27 +0530

    Understand the Method Overriding by simple eg.

---
+++ Overriding/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Overriding
+{
+    public class Animal
+    {  
+        public virtual void eat()
+        {  
+            Console.WriteLine("Eating...");  
+        }  
+    }  
+    public class Dog: Animal  
+    {  
+        public override void eat()  
+        {  
+            Console.WriteLine("Eating bread...");  
+        }  
+    }  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();  
+            d.eat();  
+        }
+    }
+}
