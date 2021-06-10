39c4ff5cbac8234011845e6c47fe7d56883dd838
shubhamnatekar <shubhamnatekar9221@gmail.com>
2021-06-10 18:26:50 +0530

    all topics added

---
+++ Generic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Generic
+{
+    //generic class
+    class GenericClass<T>  
+    {  
+        public GenericClass(T msg)  
+        {  
+            Console.WriteLine(msg);  
+        }
+    }
+
+    class Generic
+    {
+        //generic method
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);  
+        }  
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> gen   = new GenericClass<string> ("This is generic class");  
+            GenericClass<int>    genI  = new GenericClass<int>(101);  
+            GenericClass<char>   getCh = new GenericClass<char>('I');
+            Generic g1 = new Generic();
+            g1.Show("This is generic method");  
+            g1.Show(101);  
+            g1.Show('I'); 
+        }
+    }
+}
